809284eab7c848093b594bdd408bbd1d23b1a756
Elderado <underdog181@hotmail.com>
2013-02-03 15:12:10 -0800

    Update mno-assignments/scripts/orders.sql

--- mno-assignments/scripts/orders.sql
+++ mno-assignments/scripts/orders.sql
@@ -4,12 +4,13 @@ CREATE TABLE orders
 `vID` int(10) NOT NULL,
 `vendor` varchar(12) NOT NULL,
 `orderID` int(20) NOT NULL,
+`orderDate` DATE NOT NULL,
   PRIMARY KEY (`vID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO orders (vID, vendor, orderID) 
- VALUES ('1212', 'Coca-Cola', '5440');
-INSERT INTO orders (vID, vendor, orderID)
- VALUES ('1213', 'Vector Inc', '699605');
-INSERT INTO orders (vID, vendor, orderID)
- VALUES ('1214', 'Mann Co', '3');
+INSERT INTO orders (vID, vendor, orderID, orderDate) 
+ VALUES ('1212', 'Coca-Cola', '5440', '1992-06-04');
+INSERT INTO orders (vID, vendor, orderID, orderDate)
+ VALUES ('1213', 'Vector Inc', '699605', '1993-12-12');
+INSERT INTO orders (vID, vendor, orderID, orderDate)
+ VALUES ('1214', 'Mann Co', '3', '2012-12-12');
