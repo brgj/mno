c77e3d184d60fe508e4254474d600bbb207cb149
Elderado <underdog181@hotmail.com>
2013-02-03 12:54:43 -0800

    Create add_order_form.php
    View for order-adding page.

---
+++ mno-assignments/application/views/add_order_form.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * @author Alexandra Kabak
+ * PO
+ * order form View
+ */
+    $this->load->helper('form');
+    echo form_open('/add_order/post');
+    echo form_label('Vendor','vID');
+    echo form_input('vID','{vID}')."\r\n";
+    echo form_label('Vendor Name','vendor');
+    echo form_input('vendor','{vendor}')."\r\n";
+    echo form_label('Order ID', 'orderID');
+    echo form_input('orderID', '{orderID}')."\r\n";
+    
+    echo form_submit(null, 'Submit');
+    echo form_close();
+    
+?>
+
