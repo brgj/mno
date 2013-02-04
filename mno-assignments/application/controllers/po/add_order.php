bdc3c9fece942c48d6ecffe9da83a62a6c502120
LuciferFayte <luciferfayte@hotmail.com>
2013-02-03 16:14:20 -0800

    Update mno-assignments/application/controllers/po/add_order.php
    added date field and validation

--- mno-assignments/application/controllers/po/add_order.php
+++ mno-assignments/application/controllers/po/add_order.php
@@ -21,7 +21,7 @@ class Add_order extends Application {
 
     function index() {
         $this->data['selected'] = '/po/add_order';
-        $record = array('vID' => '', 'vendor' => '', 'orderID' => '');
+        $record = array('vID'=>'', 'vendor'=>'', 'orderID'=>'','orderDate'=>'');
         $this->data = array_merge($this->data, $record);
         $this->data['pagetitle'] = "Massive Noob Obliterators - Add Order";
         $this->data['pagebody'] = "po/add_order_form";
@@ -29,8 +29,23 @@ class Add_order extends Application {
     }
 
     function post() {
-        $this->orders->add($_POST);
-        redirect('/');
+        
+        $this->load->helper('po/validation_helper');
+        
+        $new_id = $_POST['orderID'];
+        if ($this->orders->get($new_id) != null)
+            $this->data['errors'][] = 'Order ID already in use';
+        
+        if (!date_validation($_POST['orderDate']))
+            $this->data['errors'][] = 'Invalid date; Usage: YYYY/MM/DD';
+        
+        if(count($this->data['errors']) > 0)
+            $this->index();  
+        else
+        {
+            $this->orders->add($_POST);
+            redirect('/');     
+        }
     }
 
 }
