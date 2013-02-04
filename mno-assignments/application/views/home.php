7dd8229f374ad4e0a8977121112a2f91e1d96e4a
brgj <brad.johnson155@gmail.com>
2013-02-04 04:04:00 +0000

    Changed default text on home

--- mno-assignments/application/views/home.php
+++ mno-assignments/application/views/home.php
@@ -5,14 +5,5 @@
 ?>
 <div>
     <h1>COMP4711 Internet Software Development</h1>
-    My development environment:
-    <ul>
-        <li>Operating system: <a href="http://openindiana.org/" target="_blank">OpenIndiana oi151</a></li>
-        <li>AMP distribution: <a href="http://www.apachefriends.org/en/xampp-solaris.html" target="_blank">XAMPP 1.7.7</a></li>
-        <li>Apache: <a href="http://httpd.apache.org" target="_blank">2.2.21</a></li>
-        <li>PHP: <a href="http://www.php.net/" target="_blank">5.3.8</a></li>
-         <li>Web document base: /pub7/htdocs</li>
-        <li>XAMPP configuration files: /opt/xampp/etc</li>
-        <li>XAMPP error logs: /opt/xampp/logs</li>
-    </ul>
+    We massively obliterate noobs. Or obliterate massive noobs. Whatever.
 </div>
